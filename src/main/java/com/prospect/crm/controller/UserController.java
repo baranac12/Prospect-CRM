ec8b81a974d8deafabbaaf107bbe600623c1f3ed
baranac12 <baranac.1453@gmail.com>
2025-08-03 02:22:35 +0300

    fixed : UserController

--- src/main/java/com/prospect/crm/controller/UserController.java
+++ src/main/java/com/prospect/crm/controller/UserController.java
@@ -13,10 +13,13 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/v1/users")
 public class UserController {
     private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public List<UserListDto> getAllUsers() {
